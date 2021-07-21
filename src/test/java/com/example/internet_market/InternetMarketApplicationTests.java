62eba926b59364069ad6cdfd93bcdc8bc3d7843f
Admin <cypher.is.3301@gmail.com>
2021-07-21 12:09:01 +0300

    Init commit

---
+++ src/test/java/com/example/internet_market/InternetMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internet_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
