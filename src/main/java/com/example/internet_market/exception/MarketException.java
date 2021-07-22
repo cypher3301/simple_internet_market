222ab6c991787ee3e4e88df2966dd28082c63779
Admin <cypher.is.3301@gmail.com>
2021-07-22 11:19:26 +0300

    Added superclass MarketException

---
+++ src/main/java/com/example/internet_market/exception/MarketException.java
@@ -0,0 +1,15 @@
+package com.example.internet_market.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MarketException extends Exception{
+    private String message;
+
+}
