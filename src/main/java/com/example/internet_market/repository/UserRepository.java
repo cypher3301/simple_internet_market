ffe8b90e71e9ca7d1eafca0ceefceb4b56bb74cb
Admin <cypher.is.3301@gmail.com>
2021-07-26 14:50:41 +0300

    deleted unnecessary services

--- src/main/java/com/example/internet_market/repository/UserRepository.java
+++ src/main/java/com/example/internet_market/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.internet_market.repository;
 
+import com.example.internet_market.entity.Basket;
 import com.example.internet_market.entity.Provider;
 import com.example.internet_market.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
     User findByPhone(String phone);
+
 
 }
