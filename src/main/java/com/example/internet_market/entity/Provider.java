1f7e736ee49736a4e209816b51c0e6511d38e378
Admin <cypher.is.3301@gmail.com>
2021-07-27 17:26:28 +0300

    comment generation value, override special equals and hashCode for id, name

--- src/main/java/com/example/internet_market/entity/Provider.java
+++ src/main/java/com/example/internet_market/entity/Provider.java
@@ -10,11 +10,31 @@ import java.util.List;
 @Data
 public class Provider {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
 
+
+
     @OneToMany(mappedBy = "provider")
     private List<Item> itemsProvide = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Provider)) return false;
+
+        Provider provider = (Provider) o;
+
+        if (getId() != null ? !getId().equals(provider.getId()) : provider.getId() != null) return false;
+        return getName() != null ? getName().equals(provider.getName()) : provider.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
 }
