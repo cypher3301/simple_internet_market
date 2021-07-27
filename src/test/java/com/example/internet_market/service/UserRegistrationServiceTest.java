62d831cf3a5064327a107018c4643eda05411f55
Admin <cypher.is.3301@gmail.com>
2021-07-27 17:23:39 +0300

    rename registration on register, added test method register

--- src/test/java/com/example/internet_market/service/UserRegistrationServiceTest.java
+++ src/test/java/com/example/internet_market/service/UserRegistrationServiceTest.java
@@ -2,12 +2,14 @@ package com.example.internet_market.service;
 
 import com.example.internet_market.Utility;
 import com.example.internet_market.entity.User;
-import com.example.internet_market.exception.UserRegistrationException;
+import com.example.internet_market.service.exception.UserRegistrationException;
 import com.example.internet_market.repository.BasketRepository;
 import com.example.internet_market.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Objects;
 
 @SpringBootTest
 public class UserRegistrationServiceTest {
@@ -20,10 +22,22 @@ public class UserRegistrationServiceTest {
     private final User user = new Utility().generateUser();
 
     @Test
+    void register() {
+        User savedUser = userRegistrationService.register(user);
+//        User savedUser = userRepository.save(user);
+        assert savedUser.equals(user);
+        assert Objects.equals(savedUser.getId(), user.getId());
+        if (userRepository.existsById(user.getId())){
+            basketRepository.deleteById(user.getId());
+            userRepository.deleteById(user.getId());
+        }
+    }
+
+    @Test
     void registrationUserNull() {
         User localUser = null;
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("user");
             System.out.println(e.toString());
@@ -38,7 +52,7 @@ public class UserRegistrationServiceTest {
         User userRegistered;
         localUser.setId(null);
         try {
-            userRegistered = userRegistrationService.registration(localUser);
+            userRegistered = userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             System.out.println(e.toString());
             assert false;
@@ -56,7 +70,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setName(null);
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("name");
             System.out.println(e.toString());
@@ -70,7 +84,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setName("");
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("name");
             System.out.println(e.toString());
@@ -84,7 +98,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setAddress(null);
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("address");
             System.out.println(e.toString());
@@ -98,7 +112,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setAddress("");
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("address");
             System.out.println(e.toString());
@@ -112,7 +126,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setEmail(null);
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("email");
             System.out.println(e.toString());
@@ -126,7 +140,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setEmail("");
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("email");
             System.out.println(e.toString());
@@ -137,9 +151,9 @@ public class UserRegistrationServiceTest {
 
     @Test
     void registrationEmailExists() {
-        user.setPhone(user.getPhone().replace("1","2"));
+        user.setPhone(user.getPhone().replace("1", "2"));
         try {
-            userRegistrationService.registration(user);
+            userRegistrationService.register(user);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("email");
             System.out.println(e.toString());
@@ -153,7 +167,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setPhone("");
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("phone");
             System.out.println(e.toString());
@@ -167,7 +181,7 @@ public class UserRegistrationServiceTest {
         User localUser = user;
         localUser.setPhone("");
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("phone");
             System.out.println(e.toString());
@@ -179,9 +193,9 @@ public class UserRegistrationServiceTest {
     @Test
     void registrationPhoneExists() {
         User localUser = user;
-        localUser.setEmail("1"+user.getEmail());
+        localUser.setEmail("1" + user.getEmail());
         try {
-            userRegistrationService.registration(localUser);
+            userRegistrationService.register(localUser);
         } catch (UserRegistrationException e) {
             assert e.getFieldException().equals("phone");
             System.out.println(e.toString());
